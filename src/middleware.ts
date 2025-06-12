bd9f4388f32dde72fc5438bca37c8feb0d45012b
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-06-13 00:59:56 +0300

    fix: empty middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import {
   withAuth,
 } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
 
 export default withAuth(
-  async function middleware() {
+  async function middleware(req: NextRequest) {
+    return Response.next();
   },
   {
     publicPaths: ["/", "/api/public"],
